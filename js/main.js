afcdac4a2d15a06cbb01b4aca1f767f040f104c7
unknown <oopkalendar@gmail.com>
2016-07-13 13:43:55 +0200

    Created a view class for the home view

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 var app = {
-	renderHomeView: function () {
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
 	showAlert: function (message, title) {
 		if (navigator.notification) {
 			navigator.notification.alert(message, null, title, 'OK');
@@ -10,21 +6,11 @@ var app = {
 			alert(title ? (title + ": " + message) : message);
 		}
 	},
-	findByName: function () {
-		var self = this;
-		this.store.findByName($('.search-key').val(), function (employees) {
-			$('.employee-list').html(self.employeeLiTpl(employees));
-		});
-	},
 	initialize: function () {
 		var self = this;
-		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
 		this.store = new MemoryStore(function () {
-			//self.showAlert('Store Initialized', 'Info');
-			self.renderHomeView();
+			$('body').html(new HomeView(self.store).render().el);
 		});
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
 	}
 
 };
